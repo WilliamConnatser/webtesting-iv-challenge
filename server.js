3beb3c08565bf845bc3afe433cf45c05a0f7713c
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-05-16 21:02:38 -0500

    mvp finished

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 const server = express();
-server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.statusCode(200).send({ api: "up" });
-});
+server.use(express.json());
+server.use('/',routes);
 
 server.listen(5000, () => console.log('\n\n\u{1F680}\u{1F680}\u{1F680}\u{1F680}\u{1F680} PORT 5000 \n\n'));
 
